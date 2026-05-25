57c1d50c8bf8c6babe548d1f99da9b1efb0530c2
royer <arthur.royer.nogueira@escola.pr.gov.br>
2026-05-25 12:01:11 -0300

    ....

---
+++ script.js
@@ -0,0 +1,87 @@
+// ==========================
+// CONTROLE DE ABAS
+// ==========================
+
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    };
+}
+
+
+// ==========================
+// DEFINIÇÃO DOS ALVOS
+// ==========================
+
+const contadores = document.querySelectorAll(".contador");
+
+const tempoObjetivo1 = new Date("2026-12-28T23:59:59");
+const tempoObjetivo2 = new Date("2026-11-28T23:59:59");
+const tempoObjetivo3 = new Date("2026-10-28T23:59:59");
+const tempoObjetivo4 = new Date("2026-09-28T23:59:59");
+
+const tempos = [
+    tempoObjetivo1,
+    tempoObjetivo2,
+    tempoObjetivo3,
+    tempoObjetivo4
+];
+
+
+// ==========================
+// FUNÇÃO DE CÁLCULO
+// ==========================
+
+function calculaTempo(tempoObjetivo) {
+
+    let tempoAtual = new Date();
+
+    let tempoFinal = tempoObjetivo - tempoAtual;
+
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + " dias "
+        + horas + " horas "
+        + minutos + " minutos "
+        + segundos + " segundos";
+}
+
+
+// ==========================
+// ATUALIZAÇÃO AUTOMÁTICA
+// ==========================
+
+function atualizaContadores() {
+
+    for (let i = 0; i < contadores.length; i++) {
+
+        contadores[i].textContent =
+            calculaTempo(tempos[i]);
+
+    }
+
+}
+
+atualizaContadores();
+
+setInterval(atualizaContadores, 1000);
